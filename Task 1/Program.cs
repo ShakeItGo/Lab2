1d638caaa5f6490fba1aeaef9901c535f02da399
{{ShakeItGo}} <{{artur.potar@gmail.com}}>
2022-09-26 10:51:32 +0300

    1st commit

---
+++ Task 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Info information = new Info();
+            information.Index = 02099;
+            information.Country = "Ukraine";
+            information.City = "Kyiv";
+            information.Street = "Perova st.";
+            information.House = 123;
+            information.Apartment = 22;
+
+            Console.WriteLine($"Index: {information.Index} , Country: {information.Country} , City: {information.City} , Street: {information.Street} , House: {information.House} , Apartment: {information.Apartment}.");
+        }
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee inform = new Employee();
+            inform.Name = "John";
+            inform.Surname = "Dorian";
+            inform.Ocupation = "Junior dev.";
+            inform.Experience = 10;
+            inform.Taxes = 0.5;
+            inform.Amount = 300;
+
+            inform.Info();
+        }
+    }
+}
